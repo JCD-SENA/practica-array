78aba311bb759e56b6c2149b9f0f8a9157e91b33
Juan camilo <katuadaisnotreal@gmail.com>
2024-03-05 18:02:14 -0500

    Se muestra el largo del array

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-function generarTablas (matriz) {
-	return Array.from(matriz, (a,e) => {
-		let tabla = new Array(10)
-		for (let i = 0; i < 10; i++) {
-			tabla[i] = a * (i + 1)
-		}
-		return tabla
-	})
-}
-
-console.log(generarTablas([1, 5, 10]))
+let matriz = new Array(1, 2, 3)
+console.log(matriz.length)
